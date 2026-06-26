d9ae1064bbe50e14fac72115ef2a61a5ce585e49
Akesir <akesir22@gmail.com>
2026-06-26 17:46:01 -0300

    Complete the highest number challenge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let numbers = [7, 28, -23, 0, -11];
+    let mut highest_number = numbers[0];
+    for number in numbers {
+        if number > highest_number {
+            highest_number = number;
+        } else {}
+    }
+    println!("Highest value from the array: {}", highest_number);
 }
