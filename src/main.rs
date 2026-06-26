cd1f62200b3924f21fa0b64703b69d7bb4b3a747
Akesir <akesir22@gmail.com>
2026-06-26 17:04:57 -0300

    Complete the BMI calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let height: f64 = 1.80;
+    let weight: f64 = 70.5;
+    let bmi: f64 = weight / (height * height);
+    println!("Your BMI value is {:.1} kg/m²", bmi);
+    if bmi < 18.5 {
+        println!("That means you're underweight!");
+    } else if bmi > 25.0 {
+        println!("That means you're overweight!");
+    } else {
+        println!("That means you're healthy!");
+    }
 }
